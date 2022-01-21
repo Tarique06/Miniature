94eb384c2c37e21a4c6f0d6b2dcb6bfc0ce5b5ae
Tarique06 <tariquekhurshaidi@gmail.com>
2022-01-21 19:15:19 +0530

    initialization

---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const express = require("express")
+const cors = require("cors")
+const app = express()
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+}
+
+app.use(cors(corsOptions))
+
+
+//db.sequelize.sync({ force: true });
+
+
+app.get('/', (req, res) => {
+    res.status(200).send('Welcome to BackEnd Application.')
+})
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`)
+})
